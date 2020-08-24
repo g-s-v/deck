41a76360292a11f458462c107479c3c904882e7b
Vasiliy Briginets <0x40@yandex-team.ru>
2020-08-25 02:52:24 +0300

    feat(yandex): Support load balancer, and instance details, support stages for Yandex

--- app/scripts/modules/yandex/src/index.ts
+++ app/scripts/modules/yandex/src/index.ts
@@ -1,6 +1,2 @@
 export { YANDEX_MODULE } from './yandex.module';
 export * from './yandex.settings';
-
-// export * from './domain/index';
-// export * from './loadBalancer/index';
-// export * from './serverGroup/index';
